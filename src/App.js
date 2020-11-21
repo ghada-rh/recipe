5a046ecf87c4a0407e43ef5b85652afe4d38759c
ghada-rh <ghadarhouuma@gmail.com>
2020-11-21 13:18:09 +0100

    4 files modified

--- src/App.js
+++ src/App.js
@@ -1,64 +1,46 @@
 import React, { useState } from "react";
 import "./App.css";
 import Axios from "axios";
-import { v4 as uuidv4 } from "uuid";
 import Recipe from "./components/Recipe";
 import Alert from "./components/Alert";
 import "math";
 
-function App() {
+const App = () => {
   const [query, setQuery] = useState("");
   const [recipes, setRecipes] = useState([]);
-  const [alert, setAlert] = useState("");
 
   const APP_ID = "4e9f05eb";
   const APP_KEY = "9b904d703fa0d46a88ce1ac63f29f498";
-
   const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
   const getData = async () => {
-    if (query !== "") {
-      const result = await Axios.get(url);
-      if (!result.data.more) {
-        return setAlert("No food with such name");
-      }
-      console.log(result);
-      setRecipes(result.data.hits);
-      setQuery("");
-      setAlert("");
-    } else {
-      setAlert("Please fill the form");
-    }
+    const result = await Axios.get(url);
+    console.log(result);
+    setQuery(""); //bech tfaraghli l input ba3d mana3ml submit
+    setRecipes(result.data.hits);
   };
 
-  const onChange = e => setQuery(e.target.value);
-
+  const handleChange = e => {
+    setQuery(e.target.value);
+  };
   const onSubmit = e => {
     e.preventDefault();
     getData();
   };
-
   return (
     <div className="App">
-      <h1>Food Searching App</h1>
-      <form onSubmit={onSubmit} className="search-form">
-        {alert !== "" && <Alert alert={alert} />}
-        <input
-          type="text"
-          name="query"
-          onChange={onChange}
-          value={query}
-          autoComplete="off"
-          placeholder="Search Food"
-        />
+      <h1>RECIPES APP</h1>
+      <form className="Search-form" onSubmit={onSubmit} value={query}>
+        <input type="text" placeholder="Search Food" onChange={handleChange} />
         <input type="submit" value="Search" />
       </form>
-      <div className="recipes">
+      <div className="Recipes">
         {recipes !== [] &&
-          recipes.map(recipe => <Recipe key={Math.random()} recipe={recipe} />)}
+          recipes.map(recipe => {
+            <Recipe recipe={recipe} />;
+          })}
       </div>
     </div>
   );
-}
-
+};
 export default App;
--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,11 +1,3 @@
 import React from "react";
 
-const Alert = ({ alert }) => {
-  return (
-    <div className="alert">
-      <h3>{alert}</h3>
-    </div>
-  );
-};
-
 export default Alert;
